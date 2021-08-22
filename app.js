a0a56f5b7126915d0d401b068db9323c90ce3c80
Qhuong190239 <huongnqgch190239@fpt.edu.vn>
2021-08-22 16:44:25 +0700

    ok

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/',(req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('server is running: ', PORT)
 });
